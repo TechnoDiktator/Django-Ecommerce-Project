347d7dbc0dbb8f1190dbde3ba24ee9f21faeb47e
Tarang Rastogi <rastogitarang5@gmail.com>
2023-05-06 16:08:23 +0530

    created few models

--- shopit/app/models.py
+++ shopit/app/models.py
@@ -1,3 +1,89 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator , MinLengthValidator , MinValueValidator
+
 # Create your models here.
+
+STATE_CHOICES = (
+    ('Andaman & Niconbar Islands' , 'Andaman And Nicobar Islands'),
+    ('Andhra Pradesh','Andhra Pradesh'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chhattisgarh','Chhattisgarh'),
+    ('Goa','Goa'),
+    ('Gujarat','Gujarat'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Himachal Pradesh'),
+    ('Jammu & Kashmir','Jammu & Kashmir'),
+    ('Jharkhand','Jharkhand'),
+    ('Karnataka','Karnataka'),
+    ('Kerala','Kerala'),
+    ('Madhya Pradesh','Madhya Pradesh'),
+    ('Maharashtra','Maharashtra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya','Meghalya'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Odisha','Odisha'),
+    ('Punjab','Punjab'),
+    ('Rajasthan','Rajasthan'),
+    ('Sikkim','Sikkim'),
+    ('Tamil Nadu','Tamil Nadu'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttar Pradesh','Uttar Pradesh'),
+    ('Uttarakhand','Uttarakhand'),
+    ('West Bengal','West Bengal'),
+)
+
+
+
+
+print(type(STATE_CHOICES))
+
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices = STATE_CHOICES , max_length=50)
+
+    def __str__(self) -> str:
+        return str(self.id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
