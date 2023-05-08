af63cb6f3ee1d79f35b3ed29b84c9d0653e1ad63
Tarang Rastogi <rastogitarang5@gmail.com>
2023-05-08 17:13:39 +0530

    forgot password and password reset logic

--- shopit/app/urls.py
+++ shopit/app/urls.py
@@ -3,7 +3,7 @@ from app import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from .forms import LoginForm  , MyPasswordChangeForm
+from .forms import LoginForm  , MyPasswordChangeForm , MyPasswordResetForm,MySetPasswordForm
 
 urlpatterns = [
     #path('', views.home),
@@ -14,13 +14,21 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
-    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name ='app/changepassword.html' , form_class  = MyPasswordChangeForm ), name='changepassword'),
+    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name ='app/changepassword.html' , form_class  = MyPasswordChangeForm  , success_url = '/passwordchangedone/' ), name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
     path('mobile/<slug:data>', views.mobile, name='mobile'),
     path('login/', auth_views.LoginView.as_view(template_name = 'app/login.html', authentication_form = LoginForm  ) , name='login'),
     path('logout/'  , auth_views.LogoutView.as_view( next_page = '' )  , name='logout'),
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
+    path('passwordchangedone/' , auth_views.PasswordChangeDoneView.as_view(template_name = 'app/passwordchangedone.html')  , name='passwordchangedone'),
+    path('password-reset/' , auth_views.PasswordResetView.as_view(template_name = 'app/password_reset.html' , form_class = MyPasswordResetForm) , name='password_reset' ),
+    path('password-reset/done/' , auth_views.PasswordResetDoneView.as_view(template_name = 'app/password_reset_done.html' ) , name='password_reset_done' ),
+    path('password-reset-confirm/<uidb64>/<token>' , auth_views.PasswordResetConfirmView.as_view(template_name = 'app/password_reset_confirm.html'  , form_class = MySetPasswordForm) , name='password_reset_confirm' ),
+    path('password-reset-complete/' , auth_views.PasswordResetCompleteView.as_view(template_name = 'app/password_reset_complete.html' ) , name='password_reset_complete' ),  
+    
+    
+
 ] + static(settings.MEDIA_URL  , document_root = settings.MEDIA_ROOT)
 
 
--- shopit/shopit/settings.py
+++ shopit/shopit/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/profile/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #--------------------------------------------------
 
 # Default primary key field type
