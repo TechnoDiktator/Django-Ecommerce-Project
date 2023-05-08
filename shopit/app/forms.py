a1928729f9579b73f87c27c962bb6deaba727262
Tarang Rastogi <rastogitarang5@gmail.com>
2023-05-08 19:26:02 +0530

    smx

--- shopit/app/forms.py
+++ shopit/app/forms.py
@@ -58,18 +58,15 @@ class MySetPasswordForm(SetPasswordForm):
     
     
     
+    
+    
 class CustomerProfileForm(forms.ModelForm):
-    class Meta:
-        model = Customer
-        fields = ['name' , 'locality' , 'city' , 'state' , 'zipcode']
-        widgets = {'name':forms.TextInput(attrs={'class':'form-control'}),'locality':forms.TextInput(attrs={'class':'form-control'}),
-        'city':forms.TextInput(attrs={'class':'form-control'}),
-        'state':forms.TextInput(attrs={'class':'form-control'}),
-        'zipcode':forms.TextInput(attrs={'class':'form-control'}),}
-
-
-
-
+  class Meta:
+    model = Customer
+    fields = ['name', 'locality', 'city', 'state', 'zipcode']
+    widgets = {'name':forms.TextInput(attrs={'class':'form-control'}),'locality':forms.TextInput(attrs={'class':'form-control'}), 'city':forms.TextInput(attrs={'class':'form-control'}), 
+    'state':forms.Select(attrs={'class':'form-control'}),
+    'zipcode':forms.NumberInput(attrs={'class':'form-control'})}
 
 
 
--- shopit/app/views.py
+++ shopit/app/views.py
@@ -87,8 +87,25 @@ class CustomerRegistrationView(View):
 class ProfileView(View):
     def get(self , request):
         form = CustomerProfileForm()
-        return render(request , 'app/profile.html' , {'form':form})
-       
+        return render(request , 'app/profile.html' , {'form':form  , 'active':'btn-primary'  })
+    
+    def post(self , request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+            reg = Customer(user = usr , name = name , locality = locality  , city = city , state = state , zipcode  = zipcode)
+            reg.save()
+            messages.success(request , 'Profile Updated Successfully')
+            
+        return render(request , 'app/profile.html' , {'form' : form , 'active':'btn-primary'})
+            
+        
+        
 
 
 
